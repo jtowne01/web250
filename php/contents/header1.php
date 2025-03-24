f25592163de2cddf29552133d42e1dbd7fe26bb3
jtowne01 <151704673+jtowne01@users.noreply.github.com>
2025-03-24 17:11:44 -0400

    Add files via upload

---
+++ php/contents/header1.php
@@ -0,0 +1,29 @@
+<header>
+        <h1 class="header-title">
+        Jason Towne's Jazzy Tiger | WEB250
+        </h1>
+    <nav>
+            <ul>
+        <li><a href="?p=index">Home</a></li> 
+        <li><a href="?p=introduction">Introduction</a></li> 
+        <li><a href="?p=contract">Course Contract</a></li> 
+        <li><a href="?p=names">Names</a></li>
+        <li><a href="/joy/samsusedcars.html">Jazzy Tiger's Used Cars</a></li>
+        <li>
+    <a href="#">Site Version &#x25BE;</a>
+    <ul class="dropdown">
+    <li><a href="https://jtowne01.github.io/web250/">Static Page</a></li>
+    <li><a href="https://jtowne.great-site.net/index.php?i=1">Dynamic Page</a></li>
+    </ul>
+    </li>
+                <li>
+        <a href="#">External Pages &#x25BE;</a>
+    <ul class="dropdown">
+        <li><a href="/multipage_sites/superduper_php/">Super Duper PHP</a></li>
+        <li><a href="/multipage_sites/superduper_static">Super Duper Static</a></li>
+        <li><a href="joy/">Joy of PHP</a></li>
+    </ul>
+</li>
+</ul>
+</nav>
+</header>
