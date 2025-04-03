cc306ca5181756140460c21f06a31eeaa5780e11
jtowne01 <151704673+jtowne01@users.noreply.github.com>
2025-04-03 17:50:35 -0400

    Add files via upload

---
+++ php/contents/contract.php
@@ -0,0 +1,6 @@
+    <h2>Course Contract</h2>
+    <p>
+      I, <a href="?p=introduction">Jason Z. Towne</a>, agree to abide by the terms in my Spring 2025 WEB250-N850 Database Driven Websites Course with my instructor, Professor D.I. von Briesen. I understand that all work that I do on my school and personal websites will be publicly available to the world. I will not put information there that is inappropriate for schoolwork, or that I wish to keep private. I also understand that it is my work that counts for attendance, not logins or showing up for class. As such, failure to turn in assignments may show as absences.
+    </p>
+    <p><em>Signed: Jason Z. Towne, 1/18/2025</em></p>
+
