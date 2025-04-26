44cb5555eb869aabaa9c058dd98e642d6571e9ce
jtowne01 <151704673+jtowne01@users.noreply.github.com>
2025-04-26 12:47:01 -0400

    Update db.php

--- php/carapp/db.php
+++ php/carapp/db.php
@@ -1,5 +1,5 @@
  <?php
-$mysqli = new mysqli('sql112.infinityfree.com', 'if0_38362188', '30vrtLokpgT', 'if0_38362188_cars' );
+$mysqli = new mysqli('' );
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
